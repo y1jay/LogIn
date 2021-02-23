70d14ebbae6bc6e0959e87248bad3e0fb9fd1652
y1jay <67768207+y1jay@users.noreply.github.com>
2021-02-23 15:59:53 +0900

    add

--- screen/Login.js
+++ screen/Login.js
@@ -2,18 +2,49 @@ import React from 'react';
 import {View, StyleSheet, Text,TextInput, TouchableOpacity,
    Image, ScrollView, ImageBackground, Dimensions} from 'react-native';
 import {withNavigation} from 'react-navigation'
+import appleAuth, {
+  AppleButton,
+  AppleAuthRequestOperation,
+  AppleAuthRequestScope,
+  AppleAuthCredentialState,
+} from '@invertase/react-native-apple-authentication';
 
-//
+
 const {height, width}= Dimensions.get("window")
 class Login extends React.Component {
   render() {
+    async function onAppleButtonPress() {
+      // performs login request
+      const appleAuthRequestResponse = await appleAuth.performRequest({
+        requestedOperation: AppleAuthRequestOperation.LOGIN,
+        requestedScopes: [AppleAuthRequestScope.EMAIL, AppleAuthRequestScope.FULL_NAME],
+      });
+    
+      // get current authentication state for user
+      // /!\ This method must be tested on a real device. On the iOS simulator it always throws an error.
+      const credentialState = await appleAuth.getCredentialStateForUser(appleAuthRequestResponse.user);
+    
+      // use credentialState response to ensure the user is authenticated
+      if (credentialState === AppleAuthCredentialState.AUTHORIZED) {
+        // user is authenticated
+      }
+    }
+
     return (
         <View style={styles.container}>
           <View style={{width:width,height:height,margin:200}}>
             <TouchableOpacity>
             <Text style={{fontSize:20}}>ㅇㅇㅇㅇ</Text>
             </TouchableOpacity>
-          
+            <AppleButton
+        buttonStyle={AppleButton.Style.BLACK}
+        buttonType={AppleButton.Type.SIGN_IN}
+        style={{
+          width: 160, // You must specify a width
+          height: 45, // You must specify a height
+        }}
+        onPress={() => onAppleButtonPress()}
+      />
           </View>
           
         </View>
